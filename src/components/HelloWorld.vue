9e0c229540093bbd70faa12157e4304b865a3a5c
StephSako <stephen.sakovitch@orange.fr>
2019-09-20 10:19:26 +0200

    mapping selection

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -36,14 +36,28 @@
             </v-list-item-content>
           </v-list-item>
         </v-card>
+
+        <v-radio-group>
+            <v-radio
+                v-for="n in 4"
+                :key="n"
+                :label="nomsMap[n-1]"
+                :value="URLMap[n-1]"
+                :checked="URLMap[n-1] === url ? 'true' : 'false'"
+                @change="showMap(n-1)"
+            ></v-radio>
+        </v-radio-group>
       </v-col>
 
       <v-col cols="12" md="4" style="height: 500px; width: 500px">
-        <l-map :zoom="zoom" :center="center">
-          <l-tile-layer :url="url" :attribution="attribution"></l-tile-layer>
-        </l-map>
+        <div class="map">
+          <l-map :zoom="zoom" :center="center">
+            <l-tile-layer :url="url" :attribution="attribution"></l-tile-layer>
+          </l-map>
+        </div>
       </v-col>
     </v-row>
+
   </v-container>
 </template>
 
@@ -71,9 +85,16 @@ export default {
 
       // Map
       zoom: 13,
+      nomsMap: ['Open Street Map', 'ArcGIS Online', 'HeiGIT', 'Wikimédia'],
+      URLMap: ['http://{s}.tile.openstreetmap.fr/osmfr/{z}/{x}/{y}.png',
+        'https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}',
+        'https://maps.heigit.org/openmapsurfer/tiles/roads/webmercator/{z}/{x}/{y}.png',
+        'https://maps.wikimedia.org/osm-intl/{z}/{x}/{y}{r}.png'],
       center: [51.505, -0.09],
-      url: 'http://{s}.tile.openstreetmap.fr/osmfr/{z}/{x}/{y}.png',
-      attribution: '&copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>'
+      url:
+        'http://{s}.tile.openstreetmap.fr/osmfr/{z}/{x}/{y}.png',
+      attribution:
+        '&copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>'
     }
   },
   methods: {
@@ -82,6 +103,12 @@ export default {
     },
     addElement () {
       this.friends.push({ name: this.name, password: this.password })
+    },
+    showMap (n) {
+      this.url = this.URLMap[n]
+    },
+    checkedURL (index) {
+      return this.URLMap[index] === this.url ? 'true' : 'false'
     }
   }
 }
@@ -89,7 +116,7 @@ export default {
 
 <style scoped>
 .map {
-height: 500px;
-width: 500px;
+  height: 450px;
+  width: 450px;
 }
 </style>
