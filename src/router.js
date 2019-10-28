4cd3d31b47383b338ab446b2d27b16be84a1b5b6
StephSako <stephen.sakovitch@orange.fr>
2019-10-28 22:13:47 +0100

    components folder + /dist

--- src/router.js
+++ src/router.js
@@ -1,12 +1,12 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Home from './views/Home.vue'
-import Connexion from './views/Connexion.vue'
-import Inscription from './views/Inscription.vue'
-import Account from './views/Account.vue'
-import MyEvents from './views/MyEvents.vue'
-import AddEvent from './views/AddEvent.vue'
-import ParticipateEvents from './views/ParticipateEvents.vue'
+import Home from './components/Home.vue'
+import Connexion from './components/Connexion.vue'
+import Inscription from './components/Inscription.vue'
+import Account from './components/Account.vue'
+import MyEvents from './components/MyEvents.vue'
+import AddEvent from './components/AddEvent.vue'
+import ParticipateEvents from './components/ParticipateEvents.vue'
 Vue.use(Router)
 
 export default new Router({
